491d92c6956937cc7d364bb0789f23f00fd9b95a
Vishnu Prashanth <prashanthvishnu2006@gmail.com>
2026-01-08 20:45:00 +0530

    confi file

--- src/processing/range_doppler.py
+++ src/processing/range_doppler.py
@@ -20,4 +20,24 @@ class RangeDopplerSAR:
 
         self.config = config
         self.c = 3e8  # Speed of light in [m/s]
-    
+
+    def range_compression(self, raw_data: np.ndarray) -> np.ndarray:
+        """
+        Perform range compression (FFT along fast-time)
+        
+        Args:
+            raw_data: complex 2D raw data of shape (num_samples, num_chirps)
+
+        Returns:
+            Range compressed data(profiles) of shape (num_samples, num_chirps)
+        """
+        # Apply windowing(Hamming) to reduce sidelobes
+        window = np.hamming(raw_data.shape[0])
+        windowed_data = raw_data * window[:, np.newaxis]
+
+        # FFT along range dimension (axis=0)
+        range_profiles = np.fft.fft(windowed_data, axis=0)
+        range_profiles = np.fft.fftshift(range_profiles, axes=0)
+
+        return range_profiles
+        
